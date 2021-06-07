31575a06a86f6dcafef9aa0bd6e058b625e6a408
apinto <apinto@42lisboa.com>
2021-06-07 10:11:12 +0100

    fixes negatives in content is int 🪐

--- ft_content_is_int.c
+++ ft_content_is_int.c
@@ -6,7 +6,7 @@
 /*   By: apinto <apinto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 09:23:21 by apinto            #+#    #+#             */
-/*   Updated: 2021/06/07 05:44:26 by apinto           ###   ########.fr       */
+/*   Updated: 2021/06/07 10:10:45 by apinto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_content_is_int(char	*content)
 	int sign;
 
 	sign = 0;
+	if (ft_strcmp(content, "-") == 0)
+		return (0);
 	while (*content)
 	{
 		if ((!(ft_isdigit(*content)) && *content != '-') ||
