e8c953a7637248a82019f22e79b7e351b8a95c83
protsaq <61233724+protsaq@users.noreply.github.com>
2021-07-15 16:05:37 +0100

    fixes content_is_int w/ the help of drop senpai 🎎

--- ft_content_is_int.c
+++ ft_content_is_int.c
@@ -9,19 +9,54 @@
 /*   Updated: 2021/07/15 09:13:39 by apinto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft.h"
+
+static int	ft_isspace(int chr)
+{
+	return (chr == 9 || chr == 10 || chr == 11
+		|| chr == 12 || chr == 13 || chr == 0 || chr == ' ');
+}
+
+static int	ft_issign(char chr)
+{
+	return (chr == '-' || chr == '+');
+}
+
+static int	ft_turbo_atoi(const char *str, int *error)
+{
+	int	sign;
+	int	res;
+
+	sign = 1;
+	while (ft_isspace(*str))
+		str++;
+	if (ft_issign(*str))
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	res = 0;
+	while (ft_isdigit(*str))
+	{
+		if ((res * 10 + (*(str) - 48)) < res)
+			*error = 1;
+		res = res * 10 + (*(str++) - 48);
+	}
+	if (*str && !ft_isdigit(*str))
+		*error = 1;
+	return (res * sign);
+}
 
 int	ft_content_is_int(char *src)
 {
-	int		size;
 	int		value;
-	char	*number_in_string;
+	int		error;
 
-	size = ft_strlen(src);
-	number_in_string = ft_itoa(ft_atoi(src));
-	value = (!(size != ft_strlen(number_in_string)
-		|| ft_strncmp(src, number_in_string, size)));
-	free(number_in_string);
-	return (value);
+	error = 0;
+	value = ft_turbo_atoi(src, &error);
+	if (!error)
+		return (1);
+	else
+		return (0);
 }
